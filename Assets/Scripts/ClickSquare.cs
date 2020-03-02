cf3541e9e5aaf782f3c9d9fe2bc88d0c4bee3f97
Lachlan Ridley <lachie.ridley@gmail.com>
2020-03-02 21:47:09 +1300

    Initial Commit

---
+++ Assets/Scripts/ClickSquare.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSquare : MonoBehaviour
+{
+    public Sprite cardTexture;
+    public Sprite backTexture;
+    public int value;
+
+    public PairChecker pairChecker;
+
+    private SpriteRenderer m_SpriteRenderer;
+    private Transform m_Transform;
+
+    private int m_FlipStage = 0;
+    private bool m_ToldPairChecker = false;
+
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_Transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (m_FlipStage == 1 && m_Transform.localScale.x > 0f)
+        {
+            m_Transform.localScale -= new Vector3(0.08f, 0f);
+        }
+        else if (m_FlipStage == 1 && m_Transform.localScale.x <= 0f)
+        {
+            m_FlipStage = 2;
+            m_SpriteRenderer.sprite = cardTexture;
+        }
+        else if (m_FlipStage == 2 && m_Transform.localScale.x < 2f)
+        {
+            m_Transform.localScale += new Vector3(0.08f, 0f);
+        }
+        else if (m_FlipStage == 2 && m_Transform.localScale.x >= 2f && !m_ToldPairChecker)
+        {
+            pairChecker.SelectCard(this);
+            m_ToldPairChecker = true;
+        }
+        else if (m_FlipStage == 3 && m_Transform.localScale.x > 0f)
+        {
+            m_Transform.localScale -= new Vector3(0.08f, 0f);
+        }
+        else if (m_FlipStage == 3 && m_Transform.localScale.x <= 0f)
+        {
+            m_FlipStage = 4;
+            m_SpriteRenderer.sprite = backTexture;
+        }
+        else if (m_FlipStage == 4 && m_Transform.localScale.x < 2f)
+        {
+            m_Transform.localScale += new Vector3(0.08f, 0f);
+        }
+        else if (m_FlipStage == 4 && m_Transform.localScale.x >= 2f)
+        {
+            m_FlipStage = 0;
+        }
+    }
+
+    public void FlipBack()
+    {
+        if (m_FlipStage == 2)
+        {
+            m_FlipStage = 3;
+            m_ToldPairChecker = false;
+        }
+    }
+    void OnMouseDown()
+    {
+        if (m_FlipStage == 0)
+        {
+            m_FlipStage = 1;
+        } else if (m_FlipStage == 2)
+        {
+            FlipBack();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PairChecker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PairChecker : MonoBehaviour
+{
+    private ClickSquare m_SelectionA;
+    private ClickSquare m_SelectionB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SelectCard(ClickSquare clickSquare)
+    {
+        if (m_SelectionA == null)
+        {
+            m_SelectionA = clickSquare;
+        }
+        else
+        {
+            m_SelectionB = clickSquare;
+            // TODO check if valid match
+            if (m_SelectionA.value == m_SelectionB.value)
+            {
+                Destroy(m_SelectionA.gameObject);
+                Destroy(m_SelectionB.gameObject);
+            }
+            else
+            {
+                // TODO add a pause here
+                m_SelectionA.FlipBack();
+                m_SelectionA = null;
+
+                m_SelectionB.FlipBack();
+                m_SelectionB = null;
+            }
+        }
+    }
+}
